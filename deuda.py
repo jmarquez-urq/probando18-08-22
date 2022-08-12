c690ec57425db1b2064f9e93938027394d98907b
Juan Marquez <jmarquez.urquiza@gmail.com>
2022-08-12 20:48:47 -0300

    Resuelve el ejercicio

--- deuda.py
+++ deuda.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
 deuda = 1000
+while deuda > 0:
+    pago = int(input("Monto de la cuota: "))
+    deuda = deuda - pago
+    print("Resta pagar $", deuda)
+
+print("Deuda saldada!")
+if deuda < 0:
+    print(f"Se pagaron ${-deuda} demás")
